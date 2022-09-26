e151e3bfab845be6fd1ab4471438a98d6f25b742
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-09-26 10:09:25 +1000

    Add cart-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { CartFormComponent } from './cart-form/cart-form.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductListComponent,
     TopBarComponent,
     ShippingComponent,
+    CartFormComponent,
   ],
   imports: [
     AppRoutingModule,
